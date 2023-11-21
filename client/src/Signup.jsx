403c9d96c88b732c77d4f032303a58f86b279a68
DESKTOP-VVN7SIU\User <nunnapatppp@gmail.com>
2023-11-21 09:53:47 +0700

    write sign up page

---
+++ client/src/Signup.jsx
@@ -0,0 +1,81 @@
+// SignUp.js
+import React, { useState } from "react";
+
+const SignUp = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your signup logic here
+    console.log("Form submitted:", formData);
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          First Name:
+          <input
+            type="text"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+
+        <label>
+          Last Name:
+          <input
+            type="text"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+
+        <button type="submit">Sign Up</button>
+        <button type="submit">Log In</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
